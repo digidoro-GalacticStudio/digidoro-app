0a091516767d7140f55c5c659c50a5570ad389bc
HenryLima07 <00033721@uca.edu.sv>
2023-05-10 11:54:57 -0600

    DIGI-9 adding colors and theme

--- app/src/main/java/com/galacticstudio/digidoro/ui/theme/Color.kt
+++ app/src/main/java/com/galacticstudio/digidoro/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package com.galacticstudio.digidoro.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+//theme colors
+val White60 = Color(0xFFFFFFFF)
+val Gray30 = Color(0xFF202124)
+val AzureBlue10 = Color(0xFF4880FF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Gray60 = Color(0xFF202124)
+val BabyGray30 = Color(0xFFE8EAED)
+
+
+//accent color
+val Cherry_red_accent = Color(0xFFE15A51)
+val Mint_accent = Color(0xFF85E0A3)
+val Azure_light_blue_accent = Color(0xFF8CC6E7)
+val Honey_yellow_accent = Color(0xFFFFC700)
+val Lavender_accent = Color(0xFFC7B9FF)
