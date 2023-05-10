818473e62fa04b0f6517ff7fae726a0e22e28bd3
HenryLima07 <00033721@uca.edu.sv>
2023-05-10 11:55:41 -0600

    DIGI-7 add project typography

--- app/src/main/java/com/galacticstudio/digidoro/ui/theme/Type.kt
+++ app/src/main/java/com/galacticstudio/digidoro/ui/theme/Type.kt
@@ -6,29 +6,145 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
+//setting Styles
+val DisplayLarge = TextStyle(
+    fontFamily = Nunito,
+    fontWeight = FontWeight.ExtraBold,
+    fontSize = 57.sp,
+    lineHeight = 64.sp,
+    letterSpacing = (0).sp
+
+)
+val DisplayMedium = TextStyle(
+    fontFamily = Nunito,
+    fontWeight = FontWeight.Bold,
+    fontSize = 48.sp,
+    lineHeight = 52.sp,
+    letterSpacing = (0).sp
+
+)
+val DisplaySmall = TextStyle(
+    fontFamily = Nunito,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 36.sp,
+    lineHeight = 44.sp,
+    letterSpacing = (0).sp
+
+)
+val HeadlineLarge = TextStyle(
+    fontFamily = Nunito,
+    fontWeight = FontWeight.Bold,
+    fontSize = 32.sp,
+    lineHeight = 40.sp,
+    letterSpacing = (0).sp
+
+)
+val HeadlineMedium = TextStyle(
+    fontFamily = Nunito,
+    fontWeight = FontWeight.Medium,
+    fontSize = 28.sp,
+    lineHeight = 36.sp,
+    letterSpacing = (0).sp
+
+)
+val HeadlineSmall = TextStyle(
+    fontFamily = Nunito,
+    fontWeight = FontWeight.Normal,
+    fontSize = 24.sp,
+    lineHeight = 32.sp,
+    letterSpacing = (0).sp
+
+)
+val TitleLarge = TextStyle(
+    fontFamily = Nunito,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 22.sp,
+    lineHeight = 28.sp,
+    letterSpacing = (0).sp
+
+)
+val TitleMedium = TextStyle(
+    fontFamily = Nunito,
+    fontWeight = FontWeight.Medium,
+    fontSize = 16.sp,
+    lineHeight = 24.sp,
+    letterSpacing = (0).sp
+
+)
+val TitleSmall = TextStyle(
+    fontFamily = Nunito,
+    fontWeight = FontWeight.Medium,
+    fontSize = 14.sp,
+    lineHeight = 20.sp,
+    letterSpacing = (0).sp
+
+)
+val LabelLarge = TextStyle(
+    fontFamily = Inter,
+    fontWeight = FontWeight.Medium,
+    fontSize = 14.sp,
+    lineHeight = 20.sp,
+    letterSpacing = (0).sp
+
+)
+val LabelMedium = TextStyle(
+    fontFamily = Inter,
+    fontWeight = FontWeight.Medium,
+    fontSize = 12.sp,
+    lineHeight = 16.sp,
+    letterSpacing = (0).sp
+
+)
+val LabelSmall = TextStyle(
+    fontFamily = Inter,
+    fontWeight = FontWeight.Medium,
+    fontSize = 11.sp,
+    lineHeight = 16.sp,
+    letterSpacing = (0).sp
+
+)
+val BodyLarge = TextStyle(
+    fontFamily = Inter,
+    fontWeight = FontWeight.W400,
+    fontSize = 16.sp,
+    lineHeight = 24.sp,
+    letterSpacing = (0).sp
+
+)
+val BodyMedium = TextStyle(
+    fontFamily = Inter,
+    fontWeight = FontWeight.W400,
+    fontSize = 14.sp,
+    lineHeight = 20.sp,
+    letterSpacing = (0).sp
+
+)
+val BodySmall = TextStyle(
+    fontFamily = Inter,
+    fontWeight = FontWeight.W400,
+    fontSize = 12.sp,
+    lineHeight = 16.sp,
+    letterSpacing = (0).sp
+
+)
+
+
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+    displayLarge = DisplayLarge,
+    displayMedium = DisplayMedium,
+    displaySmall = DisplaySmall,
+    headlineLarge = HeadlineLarge,
+    headlineMedium = HeadlineMedium,
+    headlineSmall = HeadlineSmall,
+    titleLarge = TitleLarge,
+    titleMedium = TitleMedium,
+    titleSmall = TitleSmall,
+    labelLarge = LabelLarge,
+    labelMedium = LabelMedium,
+    labelSmall = LabelSmall,
+    bodyLarge = BodyLarge,
+    bodyMedium = BodyMedium,
+    bodySmall = BodySmall,
+
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
-)
