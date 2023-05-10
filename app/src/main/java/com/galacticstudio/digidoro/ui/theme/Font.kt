cca1c06bb9249a7aa11a0074a67cb2293ae3b837
HenryLima07 <00033721@uca.edu.sv>
2023-05-10 11:47:36 -0600

    DIGI-7 add project fonts and themes

---
+++ app/src/main/java/com/galacticstudio/digidoro/ui/theme/Font.kt
@@ -0,0 +1,34 @@
+package com.galacticstudio.digidoro.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.galacticstudio.digidoro.R
+
+
+//Nunito Font
+val Nunito = FontFamily(
+    Font(R.font.nunito_extralight, FontWeight.W200),
+    Font(R.font.nunito_light, FontWeight.W300),
+    Font(R.font.nunito_regular, FontWeight.W400),
+    Font(R.font.nunito_medium, FontWeight.W500),
+    Font(R.font.nunito_semi_bold, FontWeight.W600),
+    Font(R.font.nunito_bold, FontWeight.W700),
+    Font(R.font.nunito_extrabold, FontWeight.W800),
+    Font(R.font.nunito_black, FontWeight.W900)
+
+)
+
+
+//Inter Font
+val Inter = FontFamily(
+    Font(R.font.inter_thin, FontWeight.W100),
+    Font(R.font.inter_extralight, FontWeight.W200),
+    Font(R.font.inter_light, FontWeight.W300),
+    Font(R.font.inter_regular, FontWeight.W400),
+    Font(R.font.inter_medium, FontWeight.W500),
+    Font(R.font.inter_semibold, FontWeight.W600),
+    Font(R.font.inter_bold, FontWeight.W700),
+    Font(R.font.inter_extrabold, FontWeight.W800),
+    Font(R.font.inter_black, FontWeight.W900)
+)
--- app/src/main/java/com/galacticstudio/digidoro/ui/theme/Theme.kt
+++ app/src/main/java/com/galacticstudio/digidoro/ui/theme/Theme.kt
@@ -16,15 +16,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Gray60,
+    secondary = BabyGray30,
+    tertiary = AzureBlue10
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = White60,
+    secondary = Gray30,
+    tertiary = AzureBlue10
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
