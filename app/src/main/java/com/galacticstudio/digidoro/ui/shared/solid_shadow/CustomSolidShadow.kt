c42870e237da3e3666c530d4a77ed2f625a57d55
HenryLima07 <00033721@uca.edu.sv>
2023-05-12 11:19:09 -0600

    DIGI-12 add solid shadow composable style

---
+++ app/src/main/java/com/galacticstudio/digidoro/ui/shared/solid_shadow/CustomSolidShadow.kt
@@ -0,0 +1,34 @@
+package com.galacticstudio.digidoro.ui.shared.solid_shadow
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+/*
+all ways applied Modifier.matchParentSize()
+*Usage:
+* Box{
+*   Shadow(addborderradius, color*, xyoffset, modifier*)
+*   element which shadow will be applied to
+* }
+* */
+
+@Composable
+fun SolidShadow(
+    borderRadius: Dp = 18.dp,
+    color: Color,
+    XYOffset:Dp = 5.dp,
+    modifier: Modifier
+){
+    Box(
+        modifier
+            .offset(XYOffset, XYOffset)
+            .background(color, shape = RoundedCornerShape(borderRadius))
+    )
+}
