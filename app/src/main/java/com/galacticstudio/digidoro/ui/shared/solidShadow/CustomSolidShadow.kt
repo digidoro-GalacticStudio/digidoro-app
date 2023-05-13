2acc27019e1f84de2ea8667acbc5426e500c060a
HenryLima07 <00033721@uca.edu.sv>
2023-05-12 22:43:32 -0600

    DIGI-12 update shadow element

--- app/src/main/java/com/galacticstudio/digidoro/ui/shared/solidShadow/CustomSolidShadow.kt
+++ app/src/main/java/com/galacticstudio/digidoro/ui/shared/solidShadow/CustomSolidShadow.kt
@@ -1,14 +1,23 @@
 package com.galacticstudio.digidoro.ui.shared.solidShadow
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.galacticstudio.digidoro.R
+import com.galacticstudio.digidoro.ui.shared.roundShadowBox.RoundShadowBox
 
 /*
 all ways applied Modifier.matchParentSize()
