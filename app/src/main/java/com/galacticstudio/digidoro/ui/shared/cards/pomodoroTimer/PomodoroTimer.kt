8ab45e75365c22eb5b3945c8a7b15786737cbc9e
HenryLima07 <00033721@uca.edu.sv>
2023-05-13 16:45:12 -0600

    DIGI-12 update cards with modifier shadow

--- app/src/main/java/com/galacticstudio/digidoro/ui/shared/cards/pomodoroTimer/PomodoroTimer.kt
+++ app/src/main/java/com/galacticstudio/digidoro/ui/shared/cards/pomodoroTimer/PomodoroTimer.kt
@@ -1,30 +1,41 @@
 package com.galacticstudio.digidoro.ui.shared.cards.pomodoroTimer
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import com.galacticstudio.digidoro.R
-import com.galacticstudio.digidoro.ui.shared.roundShadowBox.RoundShadowBox
 import com.galacticstudio.digidoro.ui.theme.DigidoroTheme
+import com.galacticstudio.digidoro.util.shadowWithCorner
 
 data class DataTime(val minutes: Int, val seconds: Int)
 
+val cornerRadius = 10.dp
 @Composable
 fun TimerBlock(time:Int){
     DigidoroTheme() {
-        RoundShadowBox(
-            shadowColor = colorResource(id = R.color.secondary_color),
-            borderRadius = 10.dp,
-            XYOffset = 5.dp,
+        Box(
             modifier = Modifier
+                .shadowWithCorner(
+                    cornerRadius = cornerRadius,
+                    shadowOffset = Offset(15.0f, 15.0f),
+                    shadowColor = colorResource(id = R.color.secondary_color)
+                )
+                .border(1.dp, colorResource(id = R.color.secondary_color), RoundedCornerShape(cornerRadius))
+                .background(MaterialTheme.colorScheme.background, RoundedCornerShape(cornerRadius))
                 .padding(horizontal = 10.dp, vertical = 6.dp)
-        ) {
+        ){
             Text(text = String.format("%02d", time), style = MaterialTheme.typography.displayLarge)
+
         }
     }
 
--- app/src/main/java/com/galacticstudio/digidoro/ui/shared/cards/todoItems/TodoItems.kt
+++ app/src/main/java/com/galacticstudio/digidoro/ui/shared/cards/todoItems/TodoItems.kt
@@ -1,9 +1,11 @@
 package com.galacticstudio.digidoro.ui.shared.cards.todoItems
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.MaterialTheme
@@ -15,32 +17,24 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import com.galacticstudio.digidoro.R
-import com.galacticstudio.digidoro.ui.shared.roundShadowBox.RoundShadowBox
+import com.galacticstudio.digidoro.util.shadowWithBorder
 
+//corner
+val cornerRadius = 5.dp
 @Composable
 fun TodoItem(todo: String, done: Boolean = false){
     var isChecked by remember { mutableStateOf(value = done) }
 
-    Box(){
-
-        RoundShadowBox(
-            shadowColor = if(isChecked) colorResource(id = R.color.secondary_color)
-            else colorResource(id = R.color.cherry_red_accent),
-            borderRadius = 5.dp,
-            XYOffset = 5.dp,
-            modifier = Modifier
-                .padding(10.dp)
-        ) {
-            TodoInformation(
-                todo = todo,
-                done = isChecked,
-                stateHandler = { isChecked = !isChecked },
-
-                )
-        }
+    Box{
+        TodoInformation(
+            todo = todo,
+            done = isChecked,
+            stateHandler = { isChecked = !isChecked },
+        )
 
     }
 }
@@ -51,25 +45,36 @@ fun TodoInformation(
     done: Boolean,
     stateHandler: ()-> Unit
 ){
-
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
+    Box(
         modifier = Modifier
-            .width(260.dp)
-    ) {
-        Checkbox(
-            checked = done,
-            colors = CheckboxDefaults.colors(
-                checkedColor = colorResource(id = R.color.gray_text_color)
-            ),
-            onCheckedChange = {
-                stateHandler()
-            } )
-        Text(
-            text = todo,
-            color = if(done == true) colorResource(id = R.color.gray_text_color) else colorResource(
-                R.color.black_text_color),
-            style = MaterialTheme.typography.labelLarge,
-        )
+            .shadowWithBorder(
+                cornerRadius = cornerRadius,
+                shadowOffset = Offset(15.0f, 15.0f),
+                shadowColor = if(done) MaterialTheme.colorScheme.secondary else colorResource(id = R.color.cherry_red_accent)
+            )
+    ){
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .width(260.dp)
+                .border(1.dp, colorResource(id = R.color.secondary_color), RoundedCornerShape(cornerRadius))
+                .background(MaterialTheme.colorScheme.background, RoundedCornerShape(cornerRadius))
+
+        ) {
+            Checkbox(
+                checked = done,
+                colors = CheckboxDefaults.colors(
+                    checkedColor = colorResource(id = R.color.gray_text_color)
+                ),
+                onCheckedChange = {
+                    stateHandler()
+                } )
+            Text(
+                text = todo,
+                color = if(done == true) colorResource(id = R.color.gray_text_color) else colorResource(
+                    R.color.black_text_color),
+                style = MaterialTheme.typography.labelLarge,
+            )
+        }
     }
 }
