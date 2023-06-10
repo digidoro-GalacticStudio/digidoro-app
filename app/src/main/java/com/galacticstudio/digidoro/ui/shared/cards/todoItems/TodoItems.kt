6a74b1c1f8763627f59d90844f305ca66fb3cdde
HenryLima07 <00033721@uca.edu.sv>
2023-06-10 17:39:33 -0600

    update composable

--- app/src/main/java/com/galacticstudio/digidoro/ui/shared/cards/todoItems/TodoItems.kt
+++ app/src/main/java/com/galacticstudio/digidoro/ui/shared/cards/todoItems/TodoItems.kt
@@ -2,8 +2,13 @@ package com.galacticstudio.digidoro.ui.shared.cards.todoItems
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Checkbox
@@ -18,31 +23,73 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.galacticstudio.digidoro.R
+import com.galacticstudio.digidoro.ui.theme.DigidoroTheme
 import com.galacticstudio.digidoro.util.shadowWithBorder
 
 //corner
 val cornerRadius = 5.dp
+data class TodoMessageData(
+    val mainMessage: String,
+    val messageBold: String,
+    val messageNoBold: String
+)
+
+@Preview(showSystemUi = true)
 @Composable
-fun TodoItem(todo: String, done: Boolean = false){
+fun todoItemPreview(){
+    DigidoroTheme() {
+        TodoItem(TodoMessageData("Tarea a realizar mada faka", "Hoy", "8:00AM"))
+    }
+}
+
+/**
+ * A composable function representing Todo Item.
+ *
+ * @param message the object of strings messages the item will display.
+ * @param done  state of the item.
+ * @param onClick function todo item will execute when click happen
+ */
+@Composable
+fun TodoItem(
+    message: TodoMessageData,
+    done: Boolean = false,
+    colorTheme: Color = MaterialTheme.colorScheme.secondary,
+    onClick: ()-> Unit = fun(){}){
     var isChecked by remember { mutableStateOf(value = done) }
 
-    Box{
+    Box(modifier = Modifier.clickable {
+        onClick
+    }){
+
         TodoInformation(
-            todo = todo,
+            message = message,
             done = isChecked,
+            colorTheme = colorTheme,
             stateHandler = { isChecked = !isChecked },
         )
 
     }
 }
 
+/**
+ * A composable function representing Data which will be display inside each item.
+ *
+ * @param message the object of strings messages the item will display.
+ * @param done  state of the item.
+ * @param stateHandler handler of item state
+ */
 @Composable
 fun TodoInformation(
-    todo: String,
+    message: TodoMessageData,
     done: Boolean,
+    colorTheme: Color,
+    modifier: Modifier = Modifier,
     stateHandler: ()-> Unit
 ){
     Box(
@@ -50,31 +97,65 @@ fun TodoInformation(
             .shadowWithBorder(
                 cornerRadius = cornerRadius,
                 shadowOffset = Offset(15.0f, 15.0f),
-                shadowColor = if(done) MaterialTheme.colorScheme.secondary else colorResource(id = R.color.cherry_red_accent)
+                shadowColor = if (done) MaterialTheme.colorScheme.secondary else colorTheme
             )
+            .then(modifier)
     ){
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
+        Column(
             modifier = Modifier
-                .width(260.dp)
-                .border(1.dp, colorResource(id = R.color.secondary_color), RoundedCornerShape(cornerRadius))
-                .background(MaterialTheme.colorScheme.background, RoundedCornerShape(cornerRadius))
+                .fillMaxWidth()
+                .border(
+                    1.dp,
+                    colorResource(id = R.color.secondary_color),
+                    RoundedCornerShape(cornerRadius)
+                )
+                .background(
+                    MaterialTheme.colorScheme.background,
+                    RoundedCornerShape(cornerRadius)
+                )
 
         ) {
-            Checkbox(
-                checked = done,
-                colors = CheckboxDefaults.colors(
-                    checkedColor = colorResource(id = R.color.gray_text_color)
-                ),
-                onCheckedChange = {
-                    stateHandler()
-                } )
-            Text(
-                text = todo,
-                color = if(done == true) colorResource(id = R.color.gray_text_color) else colorResource(
-                    R.color.black_text_color),
-                style = MaterialTheme.typography.labelLarge,
-            )
+            Row(
+                modifier = Modifier
+                    .padding(bottom = 0.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = done,
+                    colors = CheckboxDefaults.colors(
+                        checkedColor =  MaterialTheme.colorScheme.secondary,
+                        uncheckedColor = MaterialTheme.colorScheme.secondary
+                    ),
+                    onCheckedChange = {
+                        stateHandler()
+                    })
+                Text(
+                    text = message.mainMessage,
+                    color = if(done == true) MaterialTheme.colorScheme.secondary else colorResource(
+                        R.color.black_text_color),
+                    style = MaterialTheme.typography.labelLarge,
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(end = 16.dp, bottom = 8.dp),
+                horizontalArrangement = Arrangement.End
+            ) {
+                Text(
+                    text = message.messageBold + ", ",
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.labelSmall,
+                    fontWeight = FontWeight.Bold,
+                )
+                Text(
+                    text = message.messageNoBold,
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.labelSmall,
+                )
+            }
+
         }
     }
 }
