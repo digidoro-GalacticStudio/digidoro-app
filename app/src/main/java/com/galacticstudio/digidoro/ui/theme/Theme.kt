045bfcc80b27134a9f0c8d7310677e79ea050322
HenryLima07 <00033721@uca.edu.sv>
2023-06-10 17:36:36 -0600

    change theme

--- app/src/main/java/com/galacticstudio/digidoro/ui/theme/Theme.kt
+++ app/src/main/java/com/galacticstudio/digidoro/ui/theme/Theme.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -25,46 +27,17 @@ private val LightColorScheme = lightColorScheme(
     primary = White60,
     secondary = Gray30,
     tertiary = AzureBlue10
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
+
 
 @Composable
 fun DigidoroTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
-) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
-    }
-
+    content: @Composable ()-> Unit
+    ){
     MaterialTheme(
-        colorScheme = colorScheme,
         typography = Typography,
+        colorScheme = if(darkTheme) DarkColorScheme else LightColorScheme ,
         content = content
     )
 }
