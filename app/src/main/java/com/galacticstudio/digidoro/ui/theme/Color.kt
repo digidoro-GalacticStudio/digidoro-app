6a8805321fd880a9718e6c954fa6a43be7fb2ba5
cam-vasquez <00065221@uca.edu.sv>
2023-06-10 21:11:17 -0600

    :lipstick: Adding complete color palette

--- app/src/main/java/com/galacticstudio/digidoro/ui/theme/Color.kt
+++ app/src/main/java/com/galacticstudio/digidoro/ui/theme/Color.kt
@@ -6,14 +6,21 @@ import androidx.compose.ui.graphics.Color
 val White60 = Color(0xFFFFFFFF)
 val Gray30 = Color(0xFF202124)
 val AzureBlue10 = Color(0xFF4880FF)
+val OnAzureBlue = Color(0xB74880FF)
 
 val Gray60 = Color(0xFF202124)
 val BabyGray30 = Color(0xFFE8EAED)
 
+//txt color
+
 
 //accent color
-val Cherry_red_accent = Color(0xFFE15A51)
+val Cherry_accent = Color(0xFFE15A51)
+val GreenPepper_accent = Color(0xFF38A852)
 val Mint_accent = Color(0xFF85E0A3)
-val Azure_light_blue_accent = Color(0xFF8CC6E7)
-val Honey_yellow_accent = Color(0xFFFFC700)
+val BabyAzure_accent= Color(0xFF8CC6E7)
+val HoneyYellow_accent = Color(0xFFFFC700)
 val Lavender_accent = Color(0xFFC7B9FF)
+val LightGray_accent = Color(0xFFD9D9D9)
+val DarkNotes_accent = Color(0xFF3C3D41)
+val OnGray_accent = Color(0xA3D9D9D9)
--- app/src/main/java/com/galacticstudio/digidoro/ui/theme/Theme.kt
+++ app/src/main/java/com/galacticstudio/digidoro/ui/theme/Theme.kt
@@ -1,35 +1,82 @@
 package com.galacticstudio.digidoro.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Shapes
-import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
+
+    //btns y asi
     primary = Gray60,
+    onPrimary = White60,
+
     secondary = BabyGray30,
+    onSecondary = White60,
+
+
     tertiary = AzureBlue10,
-)
+    onTertiary = Gray60,
+
+    background = Gray60,
+    onBackground = Gray60,
+
+    //Surface colors affect surfaces of components, such as cards, sheets, and menus.
+    surface = Gray60,
+    onSurface = DarkNotes_accent,
+    surfaceVariant = OnGray_accent, //menu's color hover
+    onSurfaceVariant = DarkNotes_accent, //notes background
+
+
+
+    //accents, ni modo, no hay otras variables
+    //Gray from the note taking squares
+    primaryContainer = LightGray_accent,
+    secondaryContainer = Mint_accent,
+    tertiaryContainer = BabyAzure_accent,
+    onPrimaryContainer = HoneyYellow_accent,
+    onTertiaryContainer = Lavender_accent,
+    // for pomo bar
+    error = Cherry_accent,
+    onError = GreenPepper_accent,
+
+
+
+    )
 
 private val LightColorScheme = lightColorScheme(
+    //https://m2.material.io/design/color/the-color-system.html#color-theme-creation
+
     primary = White60,
+    onPrimary = Gray30,
+    onPrimaryContainer = OnGray_accent, //menu's color hover
+
     secondary = Gray30,
-    tertiary = AzureBlue10
-)
+    onSecondary = White60,
+
+    tertiary = AzureBlue10,
+    onTertiary = OnAzureBlue,
+
+    background = White60,
+    onBackground = White60,
+
+    surface = White60,
+    onSurface = Gray60,
+
+    //accents, ni modo, no hay otras variables
+    //Gray from the note taking squares
+    primaryContainer = LightGray_accent,
+    secondaryContainer = Mint_accent,
+    tertiaryContainer = BabyAzure_accent,
+    surfaceVariant = HoneyYellow_accent,
+    onSurfaceVariant = Lavender_accent,
+    // for pomo bar
+    error = Cherry_accent,
+    onError = GreenPepper_accent,
+    )
+
+
 
 @Composable
 fun DigidoroTheme(
