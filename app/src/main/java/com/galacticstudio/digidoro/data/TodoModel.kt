1febe368b8c24f778ac4e40a3bc8735bee2ac178
HenryLima07 <00033721@uca.edu.sv>
2023-06-23 22:56:55 -0600

    DIGI-50 update model for todo service

--- app/src/main/java/com/galacticstudio/digidoro/data/TodoModel.kt
+++ app/src/main/java/com/galacticstudio/digidoro/data/TodoModel.kt
@@ -1,11 +1,15 @@
 package com.galacticstudio.digidoro.data
 
+import java.util.Calendar
 import java.util.Date
 
 //TODO Complete all attributes
 data class TodoModel(
+    val id: String ?= null,
     val title: String,
+    val description: String,
     val theme: String,
-    val createdAt: Date? = null,
+    val createdAt: Date ?= null,
     val state: Boolean ?= false,
+    val reminder: Date ?= null
 )
