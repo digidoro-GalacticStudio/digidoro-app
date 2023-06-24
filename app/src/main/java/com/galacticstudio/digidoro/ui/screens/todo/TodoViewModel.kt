6fb6dcd4b44d8f99a86edd77f00536ab1302dbc4
HenryLima07 <00033721@uca.edu.sv>
2023-06-23 22:59:53 -0600

    update display view

---
+++ app/src/main/java/com/galacticstudio/digidoro/ui/screens/todo/TodoViewModel.kt
@@ -0,0 +1,169 @@
+package com.galacticstudio.digidoro.ui.screens.todo
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.galacticstudio.digidoro.RetrofitApplication
+import com.galacticstudio.digidoro.domain.usecase.todo.AddTodo
+import com.galacticstudio.digidoro.domain.usecase.todo.DeleteTodo
+import com.galacticstudio.digidoro.domain.usecase.todo.GetTodos
+import com.galacticstudio.digidoro.domain.usecase.todo.TodoUseCases
+import com.galacticstudio.digidoro.network.ApiResponse
+import com.galacticstudio.digidoro.repository.TodoRepository
+import com.galacticstudio.digidoro.ui.screens.todo.components.TodosResponseState
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import java.util.Calendar
+
+
+class TodoViewModel(
+    private val todoUseCases: TodoUseCases,
+    private val repository: TodoRepository
+) : ViewModel(){
+    private val _state = mutableStateOf(TodoState())
+    val state: State<TodoState> = _state
+
+    private var apiState by mutableStateOf<TodosResponseState?>(TodosResponseState.Resume)
+
+    private val reponseEventChanel = Channel<TodosResponseState>()
+
+    val responseEvent : Flow<TodosResponseState> = reponseEventChanel.receiveAsFlow()
+
+    fun onEvent(event: TodosEvent){
+        when(event){
+            is TodosEvent.ToggleTodoState -> {
+                _state.value = state.value.copy(
+                    isOrderSectionVisible = !state.value.isOrderSectionVisible
+                )
+            }
+            is TodosEvent.Rebuild ->{
+                getAllTodo(state.value.todosOrder)
+            }
+            is TodosEvent.Order ->{
+                if(
+                    state.value.todosOrder::class == event.todoOrder::class &&
+                            state.value.todosOrder.orderType == event.todoOrder.orderType
+                ) return
+
+                getAllTodo(event.todoOrder)
+            }
+        }
+    }
+
+    private fun sendResponseEvent(event: TodosResponseState){
+        viewModelScope.launch {
+            reponseEventChanel.send(event)
+        }
+    }
+//
+//    private fun createTodo(
+//        title: String,
+//        description: String,
+//        theme: String,
+//        reminder: Calendar
+//    ){
+//        viewModelScope.launch {
+//            when(val response = repository.createTodo(
+//                title, theme, reminder, description
+//            )){
+//                is ApiResponse.Error -> {
+//                    sendResponseEvent(TodosResponseState.Error(response.exception))
+//                }
+//
+//                is ApiResponse.ErrorWithMessage -> {
+//                    sendResponseEvent(TodosResponseState.ErrorWithMessage(response.message))
+//                }
+//
+//                is ApiResponse.Success ->{
+//                    sendResponseEvent(
+//                        TodosResponseState.Success(response.data)
+//                    )
+//                }
+//            }
+//        }
+//    }
+
+    private fun getAllTodo(todosOrder: TodoOrder) {
+        viewModelScope.launch {
+            _state.value = _state.value.copy(
+                isLoading = true
+            )
+            val sortBy = when (todosOrder) {
+                is TodoOrder.Date -> "createdAt"
+                else -> ""
+            }
+            val order = when(todosOrder.orderType){
+                is OrderType.Ascending -> "asc"
+                is OrderType.Descending -> "desc"
+            }
+
+            when (val response = repository.getAllTodo(
+                sortBy = sortBy, order = order
+            )) {
+                is ApiResponse.Error -> {
+                    sendResponseEvent(TodosResponseState.Error(response.exception))
+                }
+
+                is ApiResponse.ErrorWithMessage -> {
+                    sendResponseEvent(TodosResponseState.ErrorWithMessage(response.message))
+                }
+
+                is ApiResponse.Success -> {
+                    _state.value = _state.value.copy(
+                        todos =  response.data,
+                        todosOrder = todosOrder,
+                        isLoading = false
+                    )
+                    sendResponseEvent(TodosResponseState.Success)
+                }
+            }
+        }
+    }
+
+    companion object{
+        val Factory = viewModelFactory {
+            initializer {
+                val app = this[APPLICATION_KEY] as RetrofitApplication
+                TodoViewModel(
+                    todoUseCases = TodoUseCases(
+                        getTodos = GetTodos(),
+                        deleteTodo = DeleteTodo(app.todoRepository),
+                        addTodo = AddTodo()
+                    ),
+                    repository = app.todoRepository
+                )
+            }
+        }
+    }
+
+}
+
+
+// add code to TodosOrder:
+
+sealed class TodoOrder(val orderType: OrderType){
+    class Date(orderType: OrderType): TodoOrder(orderType)
+    class Done(orderType: OrderType): TodoOrder(orderType)
+
+    fun copy(orderType: OrderType): TodoOrder{
+        return when(this){
+            is Date -> Date(orderType)
+            is Done -> Done(orderType)
+        }
+    }
+}
+
+
+sealed class OrderType{
+    object Ascending: OrderType()
+    object Descending: OrderType()
+
+}
