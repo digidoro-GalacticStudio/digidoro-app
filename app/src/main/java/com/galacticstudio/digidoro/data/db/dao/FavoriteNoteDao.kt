d1d778481b68a697b8cadf44f50167cbcb479296
HenryLima07 <00033721@uca.edu.sv>
2023-07-13 18:08:24 -0600

    adding favorite notes offline and app version 2

--- app/src/main/java/com/galacticstudio/digidoro/data/db/dao/FavoriteNoteDao.kt
+++ app/src/main/java/com/galacticstudio/digidoro/data/db/dao/FavoriteNoteDao.kt
@@ -1,5 +1,6 @@
 package com.galacticstudio.digidoro.data.db.dao
 
+import android.util.Log
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -9,6 +10,9 @@ import com.galacticstudio.digidoro.data.db.models.FavoriteNotesModelEntity
 import com.galacticstudio.digidoro.data.db.models.NoteModelEntity
 import com.galacticstudio.digidoro.network.dto.favoritenote.FavoriteNoteDao
 import com.galacticstudio.digidoro.network.dto.note.NoteData
+import com.galacticstudio.digidoro.network.dto.note.toListNoteData
+import com.galacticstudio.digidoro.network.dto.note.toListNotesId
+import com.google.gson.Gson
 
 @Dao
 interface FavoriteNoteDao{
@@ -26,34 +30,28 @@ interface FavoriteNoteDao{
 
     //set notes by id with given notes array
     @Query("UPDATE favoritenotes SET notes_id =:notes WHERE _id =:id")
-    suspend fun updateFavoriteNotesNoteById(id: String, notes: List<String>)
+    suspend fun updateFavoriteNotesNoteById(id: String, notes: List<NoteModelEntity>)
 
     //get note by id
     @Query("SELECT * FROM note WHERE _id =:id")
-    suspend fun getNoteInFavorite(id: String) : NoteModelEntity
+    suspend fun getNote(id: String) : NoteModelEntity
 
 
 //  set better update to toggle elements in array notes id
-//    @Transaction
-//    suspend fun toggleFavoriteNoteById(id: String){
-//    val folder = getFavoriteNoteById(id).notes_id
-//    val updatedFolder = folder.map{ element ->
-//        if(element._id == id) folder.filterNot { it == element }
-//        else folder + getNoteInFavorite(id)
-//    }
-//        updateFavoriteNotesNoteById(id, updatedFolder)
-//    }
-
-    //  set better update to delete elements in array notes id
-//    @Transaction
-//    suspend fun deleteNotesById(id: String){
-//        val folder = getFavoriteNoteById(id).notes_id
-//        val updatedFolder = if (folder.contains(id))
-//            folder.filterNot { it == id }
-//        else folder
-//
-//        updateFavoriteNotesNoteById(id, folder)
-//    }
+    @Transaction
+    suspend fun toggleFavoriteNoteById(noteId: String): List<String>{
+        val favoriteFolder = getFavoriteNoteWithAllNotes()[0]
+        val favoriteNotes = favoriteFolder.notes_id
+        val favoriteFolderId = favoriteFolder._id
+        val existingNote = favoriteNotes.find { it._id == noteId }
+        val updatedNotes = if(existingNote != null){
+            favoriteNotes.filterNot { it._id == noteId }
+        }
+        else favoriteNotes.plusElement(getNote(noteId))
+        updateFavoriteNotesNoteById(favoriteFolderId, updatedNotes)
+        Log.d("notes_id", "notes: " + Gson().toJson(updatedNotes))
+        return getFavoriteNoteWithAllNotes()[0].notes_id.toListNotesId()
+    }
 
     @Query("DELETE FROM favoritenotes")
     suspend fun deleteFavoriteNotes()
