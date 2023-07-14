164641197b0f143a15148f53f68aaaee5aa8618b
HenryLima07 <00033721@uca.edu.sv>
2023-07-13 19:48:44 -0600

    add create folders offline

--- app/src/main/java/com/galacticstudio/digidoro/network/dto/folder/FolderResponse.kt
+++ app/src/main/java/com/galacticstudio/digidoro/network/dto/folder/FolderResponse.kt
@@ -1,5 +1,6 @@
 package com.galacticstudio.digidoro.network.dto.folder
 
+import com.galacticstudio.digidoro.data.db.dao.FolderDao
 import com.galacticstudio.digidoro.data.db.models.FavoriteNotesModelEntity
 import com.galacticstudio.digidoro.data.db.models.FolderModelEntity
 import com.galacticstudio.digidoro.network.dto.favoritenote.FavoriteNoteResponse
@@ -83,7 +84,7 @@ fun  FolderNotesListResponse.toFolderModelEntity(): List<FolderModelEntity>{
     return response.toList()
 }
 
-fun  List<FolderModelEntity>.toFolderData(): List<FolderDataPopulated>{
+fun  List<FolderModelEntity>.toPopulatedFolderData(): List<FolderDataPopulated>{
     return map{ element ->
         FolderDataPopulated(
             id = element._id,
@@ -95,4 +96,16 @@ fun  List<FolderModelEntity>.toFolderData(): List<FolderDataPopulated>{
             updatedAt = element.updatedAt
         )
     }
+}
+fun  FolderModelEntity.toFolderData(): FolderData{
+    val arrayId = this.notes_id.map { element -> element._id }
+    return FolderData(
+            id = this._id,
+            userId = this.user_id,
+            name = this.name,
+            theme = this.theme,
+            notesId = arrayId,
+            createdAt = this.createdAt,
+            updatedAt = this.updatedAt
+        )
 }
